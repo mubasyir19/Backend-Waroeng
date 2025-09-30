e15560404e56f6423406ab25c2f547c985c87320
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-09-30 19:35:15 +0700

    add: CRUD category

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, PrismaService],
-  providers: [AppService],
+  imports: [CategoryModule],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
